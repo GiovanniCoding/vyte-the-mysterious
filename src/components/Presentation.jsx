475ca2d339e1a84a4194045604482e3b78eb46ea
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-09-26 22:01:32 -0500

    Social Media Icons added

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,6 +1,5 @@
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import profile from "../assets/profile-image.jpeg"
-import Typical from 'react-typical'
-import { Linkedin } from "../assets/Linkedin"
 
 export const Presentation = () => {
   return (
@@ -11,16 +10,20 @@ export const Presentation = () => {
           alt="Profile Image"
           className="rounded-full w-24 mb-4"
         />
-        <h1 className="mb-1 text-2xl">Giovanni Vargas</h1>
-        <Typical
+        <h1 className="mb-1 text-3xl">Giovanni Vargas</h1>
+        <h2>Machine Learning Developer</h2>
+        {/* <Typical
           steps={['', 0,
                   'Machine Learning Developer', 1000,
                   '', 0,
                   'Full Stack Developer', 1000]}
           loop={Infinity}
           wrapper='p'
-        />
-        {/* <Linkedin className='fill-tprimary' /> */}
+        /> */}
+        <div className='flex text-2xl w-24 mt-4 justify-around'>
+          <AiFillGithub className='hover:text-[#f97b8b]' />
+          <AiFillLinkedin className='hover:text-[#0e76a8]' />
+        </div>
       </div>
     </div>
   )
